4614a4c06d6576e537d9acf9dd00b3aef3212e44
Gediminas <gcilinskas@gmail.com>
2020-12-15 23:09:46 +0200

    email debugging

--- src/Service/EmailSender.php
+++ src/Service/EmailSender.php
@@ -17,24 +17,23 @@ class EmailSender
      */
     public function send(string $recipientEmail, string $recipientName, string $subject, string $body)
     {
-//        $mail = new PHPMailer(true);
-//        $mail->SMTPDebug = 2;
-//        //https://myaccount.google.com/security?pli=1#connectedapps -> to enable sending
-//        $mail->IsSMTP(); // telling the class to use SMTP
-//        $mail->SMTPAuth = true; // enable SMTP authentication
-//        $mail->SMTPSecure = 'tls';
-//        $mail->Host = "mediaport.lt"; // sets GMAIL as the SMTP server
-//        $mail->Port = 587; // set the SMTP port for the GMAIL server
-//        $mail->Username = $_ENV["GMAIL_EMAIL"];
-//        $mail->Password = $_ENV["GMAIL_PASSWORD"];
-//
-//        $mail->AddAddress($recipientEmail, $recipientName);
-//        $mail->SetFrom('gcilinskas@gmail.com', 'Gediminas');
-//        $mail->Subject = $subject;
-//        $mail->Body = $body;
+        $mail = new PHPMailer(true);
+        //https://myaccount.google.com/security?pli=1#connectedapps -> to enable sending
+        $mail->IsSMTP(); // telling the class to use SMTP
+        $mail->SMTPAuth = true; // enable SMTP authentication
+        $mail->SMTPSecure = "ssl"; // sets the prefix to the servier
+        $mail->Host = "smtp.hostinger.com"; // sets GMAIL as the SMTP server
+        $mail->Port = 587; // set the SMTP port for the GMAIL server
+        $mail->Username = $_ENV["GMAIL_EMAIL"];
+        $mail->Password = $_ENV["GMAIL_PASSWORD"];
 
-        mail($recipientEmail, $subject, $body, "From: admin@mediaport.lt");
+        $mail->AddAddress($recipientEmail, $recipientName);
+        $mail->SetFrom('gcilinskas@gmail.com', 'Gediminas');
+        $mail->Subject = $subject;
+        $mail->Body = $body;
 
-//        $mail->Send();
+//        mail($recipientEmail, $subject, $body, "From: info@mediaport.lt");
+
+        $mail->Send();
     }
 }
