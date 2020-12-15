c9e0e207781f7d8bb6a02fdcac673c93e2f79ecf
Gediminas <gcilinskas@gmail.com>
2020-12-15 22:59:28 +0200

    email debugging

--- src/Service/EmailSender.php
+++ src/Service/EmailSender.php
@@ -17,22 +17,24 @@ class EmailSender
      */
     public function send(string $recipientEmail, string $recipientName, string $subject, string $body)
     {
-        $mail = new PHPMailer(true);
-        $mail->SMTPDebug = 2;
-        //https://myaccount.google.com/security?pli=1#connectedapps -> to enable sending
-        $mail->IsSMTP(); // telling the class to use SMTP
-        $mail->SMTPAuth = true; // enable SMTP authentication
-        $mail->SMTPSecure = 'tls';
-        $mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
-        $mail->Port = 587; // set the SMTP port for the GMAIL server
-        $mail->Username = $_ENV["GMAIL_EMAIL"];
-        $mail->Password = $_ENV["GMAIL_PASSWORD"];
+//        $mail = new PHPMailer(true);
+//        $mail->SMTPDebug = 2;
+//        //https://myaccount.google.com/security?pli=1#connectedapps -> to enable sending
+//        $mail->IsSMTP(); // telling the class to use SMTP
+//        $mail->SMTPAuth = true; // enable SMTP authentication
+//        $mail->SMTPSecure = 'tls';
+//        $mail->Host = "mediaport.lt"; // sets GMAIL as the SMTP server
+//        $mail->Port = 587; // set the SMTP port for the GMAIL server
+//        $mail->Username = $_ENV["GMAIL_EMAIL"];
+//        $mail->Password = $_ENV["GMAIL_PASSWORD"];
+//
+//        $mail->AddAddress($recipientEmail, $recipientName);
+//        $mail->SetFrom('gcilinskas@gmail.com', 'Gediminas');
+//        $mail->Subject = $subject;
+//        $mail->Body = $body;
 
-        $mail->AddAddress($recipientEmail, $recipientName);
-        $mail->SetFrom('gcilinskas@gmail.com', 'Gediminas');
-        $mail->Subject = $subject;
-        $mail->Body = $body;
+        mail($recipientEmail, $subject, $body, "From: admin@mediaport.lt");
 
-        $mail->Send();
+//        $mail->Send();
     }
 }
