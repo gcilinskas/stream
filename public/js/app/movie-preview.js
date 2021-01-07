84b0b052a9251d107e5ab7f8c4935b28ded626a4
Gediminas <gcilinskas@gmail.com>
2021-01-07 19:58:44 +0200

    IE compatibility

--- public/js/app/movie-preview.js
+++ public/js/app/movie-preview.js
@@ -1,28 +1,64 @@
 import {url} from '../config.js';
 
-$(document).on("click", "[data-entry-id]", function () {
-    var movieId = $(this).data('entryId');
-    getDescription(movieId);
+// $(document).on("click", "[data-entry-id]", function () {
+//     var movieId = $(this).data('entryId');
+//     getDescription(movieId);
+//
+//     function getDescription(movie) {
+//
+//         var xmlhttp;
+//         if (window.XDomainRequest) {
+//             xmlhttp = new XDomainRequest();
+//
+//             xmlhttp.onload = function() {
+//                 console.log(xmlhttp.responseText);
+//                 document.getElementById("read-more-modal-description").innerHTML =
+//                     JSON.parse(xmlhttp.responseText).description;
+//             };
+//         } else if (window.XMLHttpRequest) {
+//             xmlhttp = new XMLHttpRequest();
+//         } else {
+//             xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//         }
+//
+//         xmlhttp.onreadystatechange = function() {
+//             if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+//                 console.log(xmlhttp.responseText);
+//                 document.getElementById("read-more-modal-description").innerHTML =
+//                     JSON.parse(xmlhttp.responseText).description;
+//             }
+//         }
+//
+//         xmlhttp.open("GET", url + '/movie/' + movie, true);
+//         xmlhttp.send();
+//     }
+// });
+// });
 
-    function getDescription(movie) {
+function readMore(movie) {
+    var xmlhttp;
+    if (window.XDomainRequest) {
+        xmlhttp = new XDomainRequest();
 
-        var xmlhttp;
-        if (window.XDomainRequest) {
-            xmlhttp=new XDomainRequest();
-            xmlhttp.onload = function(){callBack(xmlhttp.responseText)};
-        } else if (window.XMLHttpRequest) {
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                document.getElementById("read-more-modal-description").innerHTML = JSON.parse(xmlhttp.responseText).description;
-            }
-        }
-
-        xmlhttp.open("GET", url + '/movie/' + movie, true);
-        xmlhttp.send();
+        xmlhttp.onload = function() {
+            console.log(xmlhttp.responseText);
+            document.getElementById("read-more-modal-description").innerHTML =
+                JSON.parse(xmlhttp.responseText).description;
+        };
+    } else if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-});
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            console.log(xmlhttp.responseText);
+            document.getElementById("read-more-modal-description").innerHTML =
+                JSON.parse(xmlhttp.responseText).description;
+        }
+    }
+
+    xmlhttp.open("GET", url + '/movie/' + movie, true);
+    xmlhttp.send();
+}
