8aa36451b5c80a665aa6555ea86f38e8e87143d5
Gediminas <gcilinskas@gmail.com>
2021-02-04 20:30:08 +0200

    migration fix

---
+++ migrations/Version20210204182931.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210204182931 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE subscription ADD valid_from DATETIME NOT NULL, ADD valid_to DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE subscription DROP valid_from, DROP valid_to');
+    }
+}
